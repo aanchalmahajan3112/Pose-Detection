55aafc1978d4efc68c91945ec54676b61f18093e
Aanchal mahajan <98211852+aanchalmahajan3112@users.noreply.github.com>
2023-11-17 21:09:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import cv2
-import mediapipe as mp
+import mediapipe as mp     #library by Google for building various applications               
 import numpy as np
-mPose= mp.solutions.pose
-mpDraw=mp.solutions.drawing_utils
+mPose= mp.solutions.pose    #Pose class is used for pose estimation
+mpDraw=mp.solutions.drawing_utils    #DrawingUtils is used to draw the landmarks on the image.
 pose=mPose.Pose()
 
-cap=cv2.VideoCapture('1.mp4')
-drawspec1 = mpDraw.DrawingSpec(thickness=2,circle_radius=3,color=(0,0,255))
-drawspec2 = mpDraw.DrawingSpec(thickness=4,circle_radius=5,color=(0,255,0))
+cap=cv2.VideoCapture('1.mp4')          # initializes a video capture object
+drawspec1 = mpDraw.DrawingSpec(thickness=2,circle_radius=3,color=(0,0,255))   #determine appearance of landmarks on image
+drawspec2 = mpDraw.DrawingSpec(thickness=4,circle_radius=5,color=(0,255,0))   #determine appearance of connections on image  
 
-
+#while loop reads frames from the video capture object in a continuous manner
 while True:
     success,img=cap.read()
     img=cv2.resize(img,(600,700))
-    results=pose.process(img)
-    mpDraw.draw_landmarks(img,results.pose_landmarks,mPose.POSE_CONNECTIONS,drawspec1,drawspec2)
+    results=pose.process(img)   #processes image to obtain the pose landmarks
+    mpDraw.draw_landmarks(img,results.pose_landmarks,mPose.POSE_CONNECTIONS,drawspec1,drawspec2)   #landmarks drawn on img being processed
 
     h,w,c= img.shape
-    imgBlank=np.zeros([h,w,c])
-    imgBlank.fill(255)
-    mpDraw.draw_landmarks(imgBlank, results.pose_landmarks, mPose.POSE_CONNECTIONS, drawspec1, drawspec2)
+    imgBlank=np.zeros([h,w,c])      #creating a blank image
+    imgBlank.fill(255)               #filling the blank image with white color 
+    mpDraw.draw_landmarks(imgBlank, results.pose_landmarks, mPose.POSE_CONNECTIONS, drawspec1, drawspec2)  #drawing landmarks on blank img
 
 
     cv2.imshow('pose_detection', img)
